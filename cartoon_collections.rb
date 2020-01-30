33205a9becc908817d85e413d6931a6140f89315
Joe Gelay <joegelay@gmail.com>
2020-01-30 17:06:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,7 @@ def long_planeteer_calls(calls)
   calls.any?{ |call| call.length > 4 }
 end
 
-def find_the_cheese()
+def find_the_cheese(strings)
   cheese_types = ["cheddar", "gouda", "camembert"]
   
 end
